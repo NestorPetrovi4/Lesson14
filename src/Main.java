2870af42358c34315cdbb9d9485ac1ef1c81ce48
Oleg Nesterov <oleg9988@rambler.ru>
2022-08-25 16:53:11 +0300

    Начальное создание проекта

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {2, 6, 9, 43, 543, 54, 4, 99};
+        SalesManager salesManager = new SalesManager(array);
+        System.out.println(salesManager.max());
+    }
+}

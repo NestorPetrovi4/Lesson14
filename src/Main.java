35a3db034792a7ef692d5092f0df9e6d55a85ef9
Oleg Nesterov <oleg9988@rambler.ru>
2022-08-26 15:41:15 +0300

    Создан расчет среднего значения продаж

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         int[] array = {2, 6, 9, 43, 543, 54, 4, 99};
         SalesManager salesManager = new SalesManager(array);
         System.out.println(salesManager.max());
+        System.out.println(salesManager.min());
+        System.out.println(salesManager.CalculationAverage());
     }
 }
